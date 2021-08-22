ea5ad7d774ac4c1dc9462f64fcd387562e612cf4
Konstantin <diemydiesel@gmail.com>
2021-08-22 16:31:20 +0300

    bug authors

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,7 @@
 import os
+# import pathlib
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
